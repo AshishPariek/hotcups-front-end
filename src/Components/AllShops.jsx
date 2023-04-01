3294bc846d3ab199e33a7689c91e9ba058d502a0
Ashish Kumar Pareek <115872715+AshishPariek@users.noreply.github.com>
2023-04-01 12:26:28 +0530

    added_images

--- src/Components/AllShops.jsx
+++ src/Components/AllShops.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
-const AllShops = () => {
-  return <div>AllShops</div>;
+const AllShops = ({ shop }) => {
+  return (
+    <div className="shopContainer">
+      <img src={`http://localhost:4500/static/${shop.image}`} />
+    </div>
+  );
 };
 
 export default AllShops;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AllShops from "./AllShops";
 
 const Home = () => {
-  return <div className="homeContainer">Home</div>;
+  const [shops, setShops] = useState([]);
+  const [fetching, setFetching] = useState(true);
+
+  const fetcher = async () => {
+    const res = await axios.get("http://localhost:4500/hotcup/shops");
+    setShops(res.data);
+    setFetching(false);
+  };
+
+  useEffect(() => {
+    fetcher();
+  }, []);
+
+  if (fetching) {
+    return <h1>Loading...</h1>;
+  } else {
+    return (
+      <div className="homeContainer">
+        {shops.map((shop, i) => {
+          return <AllShops key={i} shop={shop} />;
+        })}
+      </div>
+    );
+  }
 };
 
 export default Home;
